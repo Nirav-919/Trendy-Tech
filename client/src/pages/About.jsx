02156590f017f85cdfa79338830d3498d2644b4a
Nirav-919 <chaturvedi.nirav@gmail.com>
2024-03-22 22:26:05 +0530

    create readme

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,21 +4,21 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Nirav' Blog
+            About Trendy Tech
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Nirav's Blog! This blog was created by Nirav Chaturvedi
-              as a personal project to share his thoughts and ideas with the
-              world. Nirav is a passionate developer who loves to write about
+              Welcome to Trendy Tech! This blog was created by me as a 
+              personal project to share my thoughts and ideas with the
+              world. I am a passionate developer who loves to write about
               technology, coding, and everything in between.
             </p>
 
             <p>
-              On this blog, you'll find weekly articles and tutorials on topics
+              On this blog, you'll find articles and tutorials on topics
               such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
-              technologies, so be sure to check back often for new content!
+              languages. I am always learning and exploring new technologies, 
+              so be sure to check back often for new content!
             </p>
 
             <p>
